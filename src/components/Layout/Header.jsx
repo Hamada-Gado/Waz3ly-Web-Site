fc2995ff42f8a76cf83fbb83c0d7266f1fccadb6
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2024-04-26 18:10:09 +0300

    feat: update header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = ({ currentPage }) => {
   const navigationLinks = [
-    { name: 'Home', to: '/' },
-    { name: 'Settings', to: '/settings' },
-    { name: 'Logout', to: '/logout' },
+    { name: "Home", to: "/" },
+    { name: "Settings", to: "/settings" },
+    { name: "Logout", to: "/logout" },
   ];
   return (
     <header className="flex justify-between items-center bg-background-dark py-4 px-6 shadow-md">
-      <div className="text-2xl font-bold">Team 92 (The Key_Strokes)</div>
-      <nav className="flex space-x-4">
-        {navigationLinks.map((link) => (
-          <Link
-            key={link.name}
-            to={link.to}
-            className={`text-text hover:text-primary font-base ${
-              currentPage === link.name ? 'underline text-secondary' : ''
-            }`}
-          >
-            {link.name}
-          </Link>
-        ))}
-      </nav>
+      <div className="img">
+        <img
+          src="../../../assets/Logo.png"
+          alt="Logo"
+          style={{ width: "10%", height: "10%" }}
+          href="/Home"
+        ></img>
+      </div>
+
+      <div className="flex text-2xl font-bold center">
+        Team 92 (The Key_Strokes) _
+      </div>
+
+      <div className="flex">
+        <nav className="space-x-4">
+          {navigationLinks.map((link) => (
+            <Link
+              key={link.name}
+              to={link.to}
+              className={`text-text hover:text-primary font-base ${
+                currentPage === link.name ? "underline text-secondary" : ""
+              }`}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
     </header>
   );
 };
