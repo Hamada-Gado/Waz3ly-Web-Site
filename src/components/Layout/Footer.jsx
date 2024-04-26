1f6c0e097f4efce3c75677d62aed4ad23593d185
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2024-04-26 19:04:21 +0300

    Feat: Footer

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex footer--pin justify-center items-center bg-background-dark py-4 px-6 shadow-md">
+      <div className="flex text-2xl font-bold center">
+        Team 92 (The Key_Strokes)
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
