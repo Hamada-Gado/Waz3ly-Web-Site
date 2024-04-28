8fbfc09a5d9ca3749908b867d8a17afbb7c8d340
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2024-04-28 15:27:26 +0300

    enhance home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-center py-4 px-6">
-        Waza3ly, for donations and more!!!
+    <div className="text-center py-4 px-6">
+      <h1 className="text-4xl font-bold text-text">
+        Waza3ly - Help That Matters
       </h1>
-      <div className="flex justify-center items-center py-4 px-6 gap-1 padding-1 shadow-sm">
+      <div className="flex justify-center items-center py-4 px-6 gap-4">
         <Link to="/Login">
-          <button className="bg-accent text-text p-2 e-round-corner font-body">
+          <button className="bg-accent text-text px-4 py-2 rounded-md shadow-sm font-body hover:bg-secondary">
             Login
           </button>
         </Link>
-
-        <button className="bg-accent text-text p-2 e-round-corner shadow-sm font-body">
+        <button className="bg-gray-200 text-gray-700 px-4 py-2 rounded-md shadow-sm font-body hover:bg-gray-300">
           Register
         </button>
       </div>
