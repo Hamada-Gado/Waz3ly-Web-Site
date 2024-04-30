f01b425fd5d7e75e1c51f6abed2e67487ec513e1
Omar Yasser <omar_yasser@Omars-MacBook-Air.local>
2024-04-30 21:18:43 +0300

    Feat: update the routing table and added a not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Admin from "./pages/Admin/Admin";
 import Organization from "./pages/Organization/Organization";
 import Donor from "./pages/Donor/Donor";
 import Header from "./components/Layout/Header";
@@ -12,6 +11,13 @@ import FurtherRegister from "./pages/Auth/FurtherRegister";
 //very imp
 import useExpandTFooter from './hooks/useExpandToFooter';
 
+// Admin imports
+import Admin from "./pages/Admin/Admin";
+import AdminListOfUsers from "./pages/Admin/ListOfUsers";
+import AdminRequests from "./components/Admin/NotFound";
+import AdminPasswords from "./components/Admin/NotFound";
+import AdminViewUser from "./components/Admin/NotFound";
+
 export default function App() {
 	const ref = useExpandTFooter();
 	return (
@@ -20,7 +26,13 @@ export default function App() {
 			<div id="main-content" ref={ref}>
 				<Routes>
 					<Route path="/" element={<Home />}></Route>
-					<Route path="/admin/*" element={<Admin />}></Route>
+
+					<Route path="/admin/" element={<Admin />}></Route>
+					<Route path="/admin/listofusers" element={<AdminListOfUsers />}></Route>
+					<Route path="/admin/requests" element={<AdminRequests />}></Route>
+					<Route path="/admin/passwords" element={<AdminPasswords />}></Route>
+					<Route path="/admin/view-user" element={<AdminViewUser />}></Route>
+					
 					<Route path="/organization/*" element={<Organization />}></Route>
 					<Route path="/donor/*" element={<Donor />}></Route>
 					<Route path="/login/*" element={<Login />}></Route>
@@ -32,3 +44,5 @@ export default function App() {
 		</Router>
 	);
 }
+
+
