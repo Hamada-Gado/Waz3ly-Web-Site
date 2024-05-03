179773f7070c5ebb8103f93394af7533c24da548
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2024-05-04 02:02:37 +0300

    minor fixes: looks nicer

--- src/components/Auth/registerThirdPage.jsx
+++ src/components/Auth/registerThirdPage.jsx
@@ -1,13 +1,9 @@
-import { AccountType, OrganizationType } from '../../enums/Enums';
+import { AccountType, OrganizationType } from "../../enums/Enums";
 
 const ThirdPage = ({ formData, ...props }) => {
   const { labelClassName, inputClassName, onChange } = { ...props };
   const selectClassName =
-    'px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-primary focus:ring-1';
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    props.onChange({ ...props.formData, [name]: value });
-  };
+    "px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-primary focus:ring-1";
   return (
     <>
       <div className="flex flex-col">
@@ -15,9 +11,9 @@ const ThirdPage = ({ formData, ...props }) => {
           Account Type:
           <select
             name="accountType"
-            defaultValue=""
+            value={formData["accountType"]}
             className={selectClassName}
-            onChange={onChange}
+            onChange={(e) => props.onChange(e)}
             required={true}
           >
             <option value="" disabled>
@@ -32,75 +28,73 @@ const ThirdPage = ({ formData, ...props }) => {
         </label>
       </div>
 
-      {formData['accountType'] === AccountType.Organization && (
+      {formData["accountType"] === AccountType.Organization && (
         <>
           <div className="flex flex-col">
             <label htmlFor="organizationType" className={labelClassName}>
               Organization Type:
+              <select
+                name="organizationType"
+                onChange={(e) => props.onChange(e)}
+                value={formData["organizationType"]}
+                className={selectClassName}
+                required
+              >
+                {Object.keys(OrganizationType).map((key) => (
+                  <option key={key} value={OrganizationType[key]}>
+                    {OrganizationType[key]}
+                  </option>
+                ))}
+              </select>
             </label>
-            <select
-              name="organizationType"
-              onChange={(e) => props.onChange(e)}
-              value={formData['organizationType']}
-              className={selectClassName}
-              required
-            >
-              {Object.keys(OrganizationType).map((key) => (
-                <option key={key} value={OrganizationType[key]}>
-                  {OrganizationType[key]}
-                </option>
-              ))}
-            </select>
           </div>
           <div className="flex flex-col">
             {/* Organization Name and address */}
             <label htmlFor="organizationName" className={labelClassName}>
               Organization Name
+              <input
+                name="organizationName"
+                type="text"
+                value={formData["organizationName"]}
+                onChange={(e) => props.onChange(e)}
+                className={inputClassName}
+                required
+              />
             </label>
-            <input
-              name="organizationName"
-              type="text"
-              value={formData['organizationName']}
-              onChange={(e) => props.onChange(e)}
-              className={inputClassName}
-              required
-            />
           </div>
           <div className="flex flex-col">
             <label htmlFor="organizationAddress" className={labelClassName}>
               Organization Address:
+              <input
+                name="organizationAddress"
+                type="text"
+                value={formData["organizationAddress"]}
+                onChange={(e) => props.onChange(e)}
+                className={inputClassName}
+                required
+              />
             </label>
-            <input
-              name="organizationAddress"
-              type="text"
-              value={formData['organizationAddress']}
-              onChange={(e) => props.onChange(e)}
-              className={inputClassName}
-              required
-            />
           </div>
         </>
       )}
-      {(formData['accountType'] === AccountType.Doctor ||
-        formData['accountType'] === AccountType.Teacher ||
-        formData['accountType'] === AccountType.Organization) && (
+      {(formData["accountType"] === AccountType.Doctor ||
+        formData["accountType"] === AccountType.Teacher ||
+        formData["accountType"] === AccountType.Organization) && (
         <>
           {/* Ask for credentials file upload */}
           <div className="flex flex-col">
             <label htmlFor="credentials" className={labelClassName}>
               Credentials:
+              <input
+                name="credentials"
+                type="file"
+                className={inputClassName}
+                required
+              />
             </label>
-            <input
-              name="credentials"
-              type="file"
-              className={inputClassName}
-              required
-            />
           </div>
         </>
       )}
-
-      <br />
     </>
   );
 };
