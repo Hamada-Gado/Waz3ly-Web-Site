1139e8efa726d91f30fbe0db06369f73a2d2bb59
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2024-05-03 19:43:29 +0300

    minor: capitalise value names

--- src/enums/Enums.js
+++ src/enums/Enums.js
@@ -3,13 +3,13 @@ export const AccountType = {
   Doctor: 'Doctor',
   Teacher: 'Teacher',
   Organization: 'Organization',
-  Reciever: 'Donor Reciever',
+  DonorReciever: 'Donor Reciever',
 };
 
 export const OrganizationType = {
-  Charity: 'charity',
-  Hospital: 'hospital',
-  PlaceOfWorship: 'place_of_worship',
-  Orphanage: 'orphanage',
-  PublicSchool: 'public_school',
+  Charity: 'Charity',
+  Hospital: 'Hospital',
+  PlaceOfWorship: 'Place Of Worship',
+  Orphanage: 'Orphanage',
+  PublicSchool: 'Public School',
 };
