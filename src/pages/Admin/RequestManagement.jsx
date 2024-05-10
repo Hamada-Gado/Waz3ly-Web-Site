7ea6550afc6480df5414762b017033b61b4f8fd5
Omar Yasser <omar_yasser@Omars-MacBook-Air.local>
2024-05-10 15:27:10 +0300

    Feat: add functionality to approve and reject requests

--- src/pages/Admin/RequestManagement.jsx
+++ src/pages/Admin/RequestManagement.jsx
@@ -1,65 +1,82 @@
+import {useState} from 'react';
 import React from 'react';
 import DataTable from 'react-data-table-component';
 
-function RequestwWindow({ onAccept, onReject }) {
-  const columns = [
-    {
-      name: 'Type',
-      selector: row => row.type,
-      sortable: true
-    },
-    {
-      name: 'Request',
-      selector: row => row.request,
-      sortable: true
-    }
-  ];
-  const data = [
-    {
-      id: 1,
-      type: 'Donor',
-      request: "clothing donation",
-    },
-    {
-      id: 2,
-      type: 'Donor',
-      request: "food donation",
-    },
-    {
-      id: 3,
-      type: 'School',
-      request: "books",
-    },
-    {
-      id: 4,
-      type: 'Hospital',
-      request: "blood donation",
-    },
-    {
-      id: 5,
-      type: 'Donor',
-      request: "money donation",
-    }
-  ]
-  return (
-    <div className='container mt-5'>
-      <DataTable
-        columns = {columns}
-        data = {data}
-        selectableRows  
-        ></DataTable>
-        <button onClick={onAccept}>Accept</button>
-        <button onClick={onReject}>Reject</button>
-    </div>
-    /*<div className="review-window">
-      <h2>Review Request</h2>
-      <p>{request}</p>
-      <div className="button-container">
-        <button onClick={onAccept}>Accept</button>
-        <button onClick={onReject}>Reject</button>
-      </div>
-    </div>*/
-  ); 
+const Data = [
+	{
+		id: 1,
+		type: 'Donor',
+		request: "clothing donation",
+	},
+	{
+		id: 2,
+		type: 'Donor',
+		request: "food donation",
+	},
+	{
+		id: 3,
+		type: 'School',
+		request: "books",
+	},
+	{
+		id: 4,
+		type: 'Hospital',
+		request: "blood donation",
+	},
+	{
+		id: 5,
+		type: 'Donor',
+		request: "money donation",
+	}
+]
+
+
+function acceptRequest(data, setData, selectedRows) {
+	// remove the request from the data
+	for (let i = 0; i < selectedRows.length; i++) {
+		data = data.filter(row => row.id !== selectedRows[i].id);
+	}
+	setData(data);
 }
 
-export default ReviewWindow;
+
+function RequestwWindow() {
+	const [selectedRows, setSelectedRows] = useState([]);
+	const [data, setData] = useState(Data); 
+	
+	const handleRowSelected = React.useCallback(state => {
+		setSelectedRows(state.selectedRows);
+	}, []);
+
+	const columns = [
+ 	 	{
+ 	 	 	name: 'Type',
+ 	 	 	selector: row => row.type,
+ 	 	 	sortable: true
+ 	 	},
+ 	 	{
+ 	 	 	name: 'Request',
+ 	 	 	selector: row => row.request,
+ 	 	 	sortable: true
+ 	 	}
+ 	];
+
+ 	return (
+ 	 	<div className='container mt-5'>
+ 	 	 	<DataTable
+ 	 	 	 	columns = {columns}
+ 	 	 	 	data = {data}
+ 	 	 	 	selectableRows 	
+ 	 	 	 	onSelectedRowsChange={handleRowSelected}
+				/ >
+ 	 	 	 	<button onClick={() => {
+						acceptRequest(data, setData, selectedRows);
+				}}>Accept</button>
+ 	 	 	 	<button onClick={() => {
+						acceptRequest(data, setData, selectedRows);
+				}}>Reject</button>
+ 	 	</div>
+ 	); 
+}
+
+export default RequestwWindow;
