5f763dd7f33f50c27869bc3909caa99e1cabeeaf
Omar Yasser <omar_yasser@Omars-MacBook-Air.local>
2024-05-11 21:50:13 +0300

    feat: finish the view user page
    FINALLYyyyyyYYYYYYYYYYYYYYYYYYYYY

--- src/pages/Admin/AdminViewUser.jsx
+++ src/pages/Admin/AdminViewUser.jsx
@@ -2,45 +2,129 @@ import {react, useState} from 'react';
 import {useLocation} from 'react-router-dom';
 import "./ViewUser.css";
 import profile from "../../../public/profilePic.png";
+import {AccountType} from "../../enums/Enums";
 
-function AdminViewUser() {
-		const location = useLocation();
-		const userId = location.state.userId;
-		const user = location.state.user;
-		console.log(Object.keys(user));
-		return (
-			<div className="container">
-				<div className="full-name">
-					{user.firstName} {user.lastName}
+
+function Table({user}) {
+	const data = {
+		id: user['id'],
+		email: user.email,
+		gender: user.gender,
+		Number: user.contactNumber,
+		area: user.area,
+		governorate: user.governorate,
+	};
+	const specialFields = {};
+
+	const accountType = user.accountType;
+	if (accountType === AccountType.Teacher) {
+		specialFields.teacherSubject = user.teacherSubject;
+	}
+	if (accountType === AccountType.Doctor) {
+		specialFields.doctorSpecialty = user.doctorSpecialty;
+	}
+
+	if (accountType === AccountType.Organization) {
+		specialFields.organizationName = user.organizationName;
+		specialFields.organizationType = user.organizationType;
+	}
+
+	if (accountType === AccountType.Teacher || accountType === AccountType.Doctor) {
+		specialFields.maxCases = user.maxCases;
+	}
+
+	// split the data into two halves
+	const keys = Object.keys(data);
+	const firstHalf = {};
+	const secondHalf = {};
+	const half = Math.ceil(keys.length / 2);
+	keys.forEach((key, index) => {
+		if (index < half) {
+			firstHalf[key] = data[key];
+		} else {
+			secondHalf[key] = data[key];
+		}
+	});
+	console.log(specialFields);
+
+	return (
+		<div className="table-container">
+			<div className="general-data">
+				<div className="general-data-header">
+					General Data
 				</div>
-				<div className="x">
-					<img src={profile} alt="pic" className="profile-pic"/>
-					<div className="info">
+				<div className="col">
+					<div className="table-header">
+						Key
 					</div>
+					{Object.keys(firstHalf).map((key) => (
+							<div className="key" key={key}>{key}</div>
+					))}
+				</div>
+				<div className="col">
+					<div className="table-header">
+						Value
+					</div>
+					{Object.values(firstHalf).map((value) => (
+							<div className="value" key={value}>{value}</div>
+					))}
+				</div>
+				<div className="col">
+					<div className="table-header">
+						Key
+					</div>
+					{Object.keys(secondHalf).map((key) => (
+							<div className="key" key={key}>{key}</div>
+					))}
+				</div>
+				<div className="col">
+					<div className="table-header">
+						Value
+					</div>
+					{Object.values(secondHalf).map((value) => (
+							<div className="value" key={value}>{value}</div>
+					))}
 				</div>
 			</div>
-		);
+			<div className="user-specific">
+				<div className="table-header-ozr">
+					Special Fields
+				</div>
+				<div className="col-ozr">
+					<div className="table-header">
+						Key
+					</div>
+					{Object.keys(specialFields).map((key) => (
+							<div className="key" key={key}>{key}</div>
+					))}
+				</div>
+				<div className="col-ozr">
+					<div className="table-header">
+						Value
+					</div>
+					{Object.values(specialFields).map((value) => (
+							<div className="value" key={value}>{value}</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function AdminViewUser() {
+	const location = useLocation();
+	const userId = location.state.userId;
+	const user = location.state.user;
+	return (
+		<div className="user-container">
+			<div className="card">
+				<img src={profile} alt="Profile" className="profile-pic"/>
+				<div className="fullname">{user.firstName} {user.lastName}</div>
+				<div className="username">{user.username} - {user.accountType}</div>
+				<Table user={user}/>
+			</div>
+		</div>
+	);
 }
 
 export default AdminViewUser;
-
-/*
-	0 "id"
-	1 "donations"
-	2 "username"
-	3 "firstName"
-	4 "lastName"
-	5 "email"
-	6 "password"
-	7 "gender"
-	8 "contactNumber"
-	9 "area"
-	10 "governorate"
-	11 "accountType"
-	12 "address"
-	13 "maxCases"
-	14 "doctorSpecialty"
-	15 "teacherSubject"
-	16 "organizationName"
-	17 "organizationType"
-*/
