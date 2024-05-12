c5f5c284b624458c7b92f4a55ce8733d665d7c75
Omar Yasser <omar_yasser@Omars-MacBook-Air.local>
2024-05-12 21:22:07 +0300

    BUG: FIX SHERLY's BUG!!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,12 @@ import Register from "./pages/Auth/Register";
 import useFitDiv from "./hooks/useFitDiv";
 import FinishedOrPending from "./pages/Donor/FinishedOrPending";
 import Settings from "./pages/Account/Settings";
-import SubmissionWindow from "./pages/Admin/SubmissionWindow";
-import RequestwWindow from "./pages/Admin/RequestManagement";
 import FilterClothes from "./components/Donor/FilterClothes";
 import FilterToys from "./components/Donor/FilterToys";
 import FilterFood from "./components/Donor/FilterFood";
 import FilterMedicalSupplies from "./components/Donor/FilterMedicalSupplies";
 import FilterTeachingPosts from "./components/Donor/FilterTeachingPosts";
 import FilterMedicalCases from "./components/Donor/FilterMedicalCases";
-import ChangePassword from "./pages/Auth/ChangePassword";
-import RequestManagement from "./pages/Admin/RequestManagement";
-import Submissions from "./pages/Admin/Submissions"
 
 // Admin imports
 import Admin from "./pages/Admin/Admin";
@@ -29,52 +24,49 @@ import RequestWindow from "./pages/Admin/RequestManagement";
 import SubmissionWindow from "./pages/Admin/SubmissionWindow";
 import AdminPasswords from "./components/Admin/NotFound";
 import AdminViewUser from "../src/pages/Admin/AdminViewUser";
-import AdminSettings from "./pages/Account/AdminSettings";
 
 export default function App() {
-  const ref = useFitDiv();
-  return (
-    <Router>
-      <Header />
-      <div id="main-content" className="h-full overflow-auto" ref={ref}>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/settings" element={<Settings />}></Route>
-          <Route path="/organization" element={<Organization />}></Route>
-          <Route path="/donor" element={<Donor />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/admin/" element={<Admin />}></Route>
-          <Route
-            path="/admin/listofusers"
-            element={<AdminListOfUsers />}
-          ></Route>
-          <Route path="/admin/requests" element={<AdminRequests />}></Route>
-          <Route path="/admin/passwords" element={<AdminPasswords />}></Route>
-          <Route path="/admin/view-user" element={<AdminViewUser />}></Route>
-          <Route
-            path="/SubmissionWindow"
-            element={<SubmissionWindow />}
-          ></Route>
-          <Route path="/RequestwWindow" element={<RequestwWindow />}></Route>
-		  <Route path="/ChangePassword" element={<ChangePassword />}></Route>
-          <Route path="/AdminSettings" element={<AdminSettings />}></Route>
-      <Route path="/Submissions" element={<Submissions />}></Route>
-          <Route
-            path="/donor/FinishedOrPendingDonations"
-            element={<FinishedOrPending />}
-          ></Route>
-          <Route
-            path="*"
-            element={
-              <div className="bg-red-500 text-white text-center p-4">
-                <h1>404 Not Found</h1>
-              </div>
-            }
-          />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
-  );
+	const ref = useFitDiv();
+	return (
+		<Router>
+			<Header />
+			<div id="main-content" className="h-full overflow-auto" ref={ref}>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/settings" element={<Settings />}></Route>
+					<Route path="/organization" element={<Organization />}></Route>
+					<Route path="/donor" element={<Donor />}></Route>
+					<Route path="/login" element={<Login />}></Route>
+					<Route path="/register" element={<Register />}></Route>
+
+					<Route path="/admin/" element={<Admin />}></Route>
+					<Route
+						path="/admin/listofusers"
+						element={<AdminListOfUsers />}
+					></Route>
+
+					<Route path="/admin/submissions" element={<SubmissionWindow />}></Route>
+					<Route path="/admin/requests" element={<RequestWindow/>}></Route>
+					<Route path="/admin/passwords" element={<AdminPasswords />}></Route>
+					<Route path="/admin/view-user" element={<AdminViewUser />}></Route>
+
+
+					<Route
+						path="/donor/FinishedOrPendingDonations"
+						element={<FinishedOrPending />}
+					></Route>
+
+					<Route
+						path="*"
+						element={
+							<div className="bg-red-500 text-white text-center p-4">
+								<h1>404 Not Found</h1>
+							</div>
+						}
+					/>
+				</Routes>
+			</div>
+			<Footer />
+		</Router>
+	);
 }
