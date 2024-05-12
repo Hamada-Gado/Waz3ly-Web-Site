1c0a6ac197c03adb622f8d725886b3a769965718
Omar Yasser <omar_yasser@Omars-MacBook-Air.local>
2024-05-12 15:29:25 +0300

    Feat: fix the delete request to make it functional

--- src/components/Admin/UserList.jsx
+++ src/components/Admin/UserList.jsx
@@ -1,23 +1,22 @@
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
+import deleteResource from "../../hooks/deleteResource";
 
 function handleView(navigate, userId, user) {
-	navigate('/admin/view-user', { state: { userId, user } });
-}
-
-function handleDelete(users, setUsers, userId) {
-	// Implement delete functionality for the user
-	console.log(`Delete user: ${userId}`);
-	// useDelete('list_of_users_for_admin', data, setData, userId); // NOT IMPLEMENTED
-	setUsers(users.filter((user) => user.id !== userId));
+	navigate("/admin/view-user", { state: { userId, user } });
 }
 
 function UserItem({ users, setUsers, user }) {
+	async function deleteUserHandler(id) {
+		await deleteResource("users", id);
+		setUsers(users.filter((user) => user.id !== id));
+	}
 	const navigate = useNavigate();
 	return (
 		<div className="flex items-center justify-between px-4 py-3 bg-gray-100 rounded-md shadow-sm hover:bg-gray-200">
 			<div className="flex items-center space-x-3">
-				<span className="text-lg font-medium">{user.firstName} {user.lastName}</span>
+				<span className="text-lg font-medium">
+					{user.firstName} {user.lastName}
+				</span>
 				<span className="text-gray-500 text-sm">{user.accountType}</span>
 			</div>
 			<div className="flex space-x-2">
@@ -30,30 +29,25 @@ function UserItem({ users, setUsers, user }) {
 						View
 					</button>
 				)}
-				{handleDelete && (
+				{
 					<button
 						type="button"
 						className="delete-but"
-						onClick={() => handleDelete(users, setUsers, user.id)}
+						onClick={() => deleteUserHandler(user.id)}
 					>
 						Delete
 					</button>
-				)}
+				}
 			</div>
 		</div>
 	);
 }
 
-function UserList ({ users , setUsers }) {
+function UserList({ users, setUsers }) {
 	return (
 		<div className="user-list">
 			{users.map((user) => (
-				<UserItem
-					key={user.id}
-					users={users}
-					setUsers={setUsers}
-					user={user}
-				/>
+				<UserItem key={user.id} users={users} setUsers={setUsers} user={user} />
 			))}
 		</div>
 	);
